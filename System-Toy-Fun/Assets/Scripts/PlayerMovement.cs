fca09b4be1cb78373ade2eac4334c650f61334ec
Jaseem P <jaseemp718@gmail.com>
2025-09-13 10:28:16 +0530

    added cinemachine TPP camera, rewind mechanism for player, blink mechanism for player

--- System-Toy-Fun/Assets/Scripts/PlayerMovement.cs
+++ System-Toy-Fun/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,22 +8,75 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveAcceleration = 5.0f;
     [SerializeField] float jumpForce = 5.0f;
+    [SerializeField] float recordLimit = 5f;
+    [SerializeField] float recordTimer = 0.0f;
+    [SerializeField] float recordInterval = 0.2f;
+    [SerializeField] float blinkBoost = 2.5f;
+    
 
     private Rigidbody rb;
-
     private Vector2 moveInput;
     private bool isJumped = false;
     private Vector3 move;
+    private bool isBlinkPressed;
+    private float dashTime = 0.2f;
+    private float dashTimer = 0f;
+
+
+    private Queue<Vector3> positionHistory = new Queue<Vector3>();
+    private Vector3 initialPosition;
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
+    void OnJump(InputValue value)
+    {
+        if (!isJumped && value.isPressed)
+        {
+            Jump();
+        }
+    }
+
+    void OnRewind(InputValue value)
+    {
+        float speed = .95f;
+        if (value.isPressed)
+        {
+            if (positionHistory.Count() > 0)
+            {
+                Vector3 past = positionHistory.Dequeue(); // one step back
+                rb.MovePosition(Vector3.Lerp(transform.position, past, speed));
+
+            }
+        }
+    }
+
+    void OnBlink(InputValue value)
+    {
+        isBlinkPressed = value.isPressed;
+    }
 
     void Movement()
     {
         move = new Vector3(moveInput.x, 0, moveInput.y);
-        rb.MovePosition(rb.position + move * moveAcceleration * Time.deltaTime);
+
+        if (isBlinkPressed)
+        {
+            dashTimer = dashTime;
+            isBlinkPressed = false;
+        }
+
+        if (dashTimer > 0)
+        {
+            rb.MovePosition(rb.position + move * moveAcceleration * blinkBoost * Time.deltaTime);
+            //isBlinkPressed = false;
+            dashTimer -= Time.deltaTime;
+        }
+        else
+        {
+            rb.MovePosition(rb.position + move * moveAcceleration * Time.deltaTime);
+        }
     }
 
     void Jump()
@@ -30,17 +86,10 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Jumped");
     }
 
-    void OnJump(InputValue value)
-    {
-        if(!isJumped && value.isPressed)
-        {
-            Jump();
-        }
-    }
 
     void OnCollisionEnter(Collision collision)
     {
-        
+
         if (collision.contacts[0].normal.y > 0.5f)
         {
             isJumped = false;
@@ -49,9 +98,31 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    void Awake() 
+    void Awake()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+
+    }
+
+    void FixedUpdate()
+    {
+        TrackPosition();
+    }
+
+    private void TrackPosition()
+    {
+        recordTimer += Time.deltaTime;
+        if (recordTimer >= recordInterval)
+        {
+            positionHistory.Enqueue(transform.position);
+            recordTimer = 0f;
+        }
+
+        if (positionHistory.Count > recordLimit / recordInterval)
+        {
+            positionHistory.Dequeue();
+        }
     }
 
     void Update()
@@ -59,5 +130,5 @@ public class PlayerMovement : MonoBehaviour
         Movement();
     }
 
-   
+
 }
